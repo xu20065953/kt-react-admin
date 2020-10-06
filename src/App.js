b6d7ac9e2cbff4e853d4f66adcff20152c0366a3
junjie.xu <20065953@163.com>
2020-10-06 11:25:42 +0800

    init one

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Button} from "antd";
 
 function App() {
   return (
     <div className="App">
         Hello React！
+        <Button type="primary">Click Me!</Button>
     </div>
   );
 }
