ff95ef657a2352e9111e5a963d84b10fd11afa41
junjie.xu <20065953@163.com>
2020-10-15 19:59:41 +0800

    cc

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,8 @@ import {
     BrowserRouter,
     Switch,
     Route,
-    Link
+    Link,
+    Redirect
 } from "react-router-dom";
 import Admin from '@/layout/admin';
 import Login from '@/pages/login';
@@ -25,6 +26,7 @@ export default class Router extends Component{
                                 <Switch>
                                     <Route path='/dashboard' component={Dashboard} />
                                     <Route path='/sys/role' component={SysRole} />
+                                    <Redirect to="/dashboard" />
                                 </Switch>
                             </Admin>
                         )
@@ -34,4 +36,41 @@ export default class Router extends Component{
             </HashRouter>
         )
     }
+}
+
+
+// 异步按需加载component
+function asyncComponent(getComponent) {
+    return class AsyncComponent extends React.Component {
+        static Component = null;
+        state = { Component: AsyncComponent.Component };
+
+        componentDidMount() {
+            if (!this.state.Component) {
+                getComponent().then(({default: Component}) => {
+                    AsyncComponent.Component = Component
+                    this.setState({ Component })
+                })
+            }
+        }
+        //组件将被卸载
+        componentWillUnmount(){
+            //重写组件的setState方法，直接返回空
+            this.setState = (state,callback)=>{
+                return;
+            };
+        }
+        render() {
+            const { Component } = this.state
+            if (Component) {
+                return <Component {...this.props} />
+            }
+            return null
+        }
+    }
+}
+
+function load(component) {
+
+    return import(/* webpackChunkName: "[request]" */`@/pages${component}`)
 }
